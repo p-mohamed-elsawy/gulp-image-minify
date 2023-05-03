32688ca72de8634da8373e1d649b29b60a74d198
p-mohamed-elsawy <dev.elsawy@gmail.com>
2023-05-03 12:31:19 +0300

    upload lib

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+import gulp from 'gulp';
+import imagemin from 'gulp-imagemin';
+import gifsicle from 'imagemin-gifsicle';
+import mozjpeg from 'imagemin-mozjpeg';
+import optipng from 'imagemin-optipng';
+import svgo from 'imagemin-svgo';
+
+export default () => (
+	gulp.src('images/*')
+        .pipe(imagemin([
+            gifsicle({interlaced: true}),
+            mozjpeg({quality: 75, progressive: true}),
+            optipng({optimizationLevel: 5}),
+            svgo({
+                plugins: [
+                    {
+                        name: 'removeViewBox',
+                        active: true
+                    },
+                    {
+                        name: 'cleanupIDs',
+                        active: false
+                    }
+                ]
+            })
+        ]))
+		.pipe(gulp.dest('dist/images'))
+);
